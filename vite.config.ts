c4cde5077dbb08f3a60a41170366684dddeb3e68
SpikeBonjour <triphuongvasa@gmail.com>
2026-05-03 18:36:54 +0700

    update vite and typescript, fixes preloading

--- vite.config.ts
+++ vite.config.ts
@@ -2,21 +2,33 @@ import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 import devtools from 'solid-devtools/vite';
-import preloadAssets from 'vite-plugin-preload-assets';
+import UnpluginInjectPreload from 'unplugin-inject-preload/vite';
 
-import bricolageRegular from "./assets/fonts/bricolage-grotesque-v9-latin_vietnamese-regular.woff2"
-import bricolageMedium from "./assets/fonts/bricolage-grotesque-v9-latin_vietnamese-600.woff2"
-import cormorant from "./assets/fonts/cormorant-garamond-v21-latin_vietnamese-regular.woff2"
+// import preloadAssets from 'vite-plugin-preload-assets';
 
 export default defineConfig({
-  plugins: [devtools(), solidPlugin(), tailwindcss(), preloadAssets({
-    preloadGoogleFonts: true,
-    fontsToPreload: [
-      { href: bricolageRegular },
-      { href: bricolageMedium },
-      { href: cormorant }
-    ],
-  })],
+  plugins: [
+    devtools(),
+    solidPlugin(),
+    tailwindcss(),
+    // preloadAssets({
+    //   fontsToPreload: [
+    //     { href: "/assets/fonts/bricolage-grotesque-v9-latin_vietnamese-regular.woff2" },
+    //     { href: "/assets/fonts/bricolage-grotesque-v9-latin_vietnamese-600.woff2" },
+    //     { href: "/assets/fonts/cormorant-garamond-v21-latin_vietnamese-regular.woff2" }
+    //   ],
+    // })
+    UnpluginInjectPreload({
+      files: [
+        {
+          entryMatch: /\/assets\/fonts\/cormorant-garamond-v21-latin_vietnamese-(regular|600)\.woff2/
+        },
+        {
+          entryMatch: /\/assets\/fonts\/cormorant-garamond-v21-latin_vietnamese-regular\.woff2/
+        }
+      ]
+    })
+  ],
   // Temporarily before moving on to a custom domain
   base: "/butcuacotam2/",
   server: {
@@ -37,5 +49,5 @@ export default defineConfig({
     },
     cssMinify: true,
   },
-  publicDir: "static"
+  publicDir: "static",
 });
